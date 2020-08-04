b98c20a767bbf88daad14ce6293a5caa35e8ac6d
JavaLearning2020 <williamalex22@126.com>
2020-08-04 15:11:41 +0800

    first commit

---
+++ JavaSE/BasicExercise/src/CourseReview/ControlStatement006/IfTest03.java
@@ -0,0 +1,17 @@
+package CourseReview.ControlStatement006;
+/*      1、系统接收一个学生的考试成绩，根据考试成绩输出成绩的等级。
+
+		2、等级：
+			优：[90~100]
+			良：[80~90) 
+			中：[70-80)
+			及格：[60~70)
+			不及格：[0-60)
+
+		3、要求成绩是一个合法的数字，成绩必须在[0-100]之间，成绩可能带有小数。
+ */
+public class IfTest03 {
+	public static void main(String[] args) {
+		
+	}
+}
