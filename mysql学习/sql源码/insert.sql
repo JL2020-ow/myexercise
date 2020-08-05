9deb2935010eb8b750cd97bb3f0b4604396f78ed
JavaLearning2020 <williamalex22@126.com>
2020-08-05 17:56:56 +0800

    first commit

---
+++ mysql学习/sql源码/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO C ( CNO, CNAME, CTEACHER ) VALUES ( '1', '语文', '张'); 
+INSERT INTO C ( CNO, CNAME, CTEACHER ) VALUES ( '2', '政治', '王'); 
+INSERT INTO C ( CNO, CNAME, CTEACHER ) VALUES ( '3', '英语', '李'); 
+INSERT INTO C ( CNO, CNAME, CTEACHER ) VALUES ( '4', '数学', '赵'); 
+INSERT INTO C ( CNO, CNAME, CTEACHER ) VALUES ( '5', '物理', '黎明'); 
+commit;
+ 
+INSERT INTO S ( SNO, SNAME ) VALUES ( '1', '学生1'); 
+INSERT INTO S ( SNO, SNAME ) VALUES ( '2', '学生2'); 
+INSERT INTO S ( SNO, SNAME ) VALUES ( '3', '学生3'); 
+INSERT INTO S ( SNO, SNAME ) VALUES ( '4', '学生4'); 
+commit;
