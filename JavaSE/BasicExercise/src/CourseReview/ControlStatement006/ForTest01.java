7c6be6580eac96a34d4cbb3d4284c8fbd8faaaa5
JavaLearning2020 <williamalex22@126.com>
2020-08-11 20:52:28 +0800

    first commit

---
+++ JavaSE/BasicExercise/src/CourseReview/ControlStatement006/ForTest01.java
@@ -0,0 +1,10 @@
+package CourseReview.ControlStatement006;
+//演示一下：为什么要使用循环
+//循环语句的出现就是为了解决代码的复用性。
+public class ForTest01 {
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			System.out.println(100);
+		}
+	}
+}
