d344ea422ece23b65101d9bc0b80bede79c4ce26
JavaLearning2020 <williamalex22@126.com>
2020-08-18 21:08:43 +0800

    first commit

--- JavaSE/BasicExercise/src/CourseReview/ControlStatement006/ForTest06.java
+++ JavaSE/BasicExercise/src/CourseReview/ControlStatement006/ForTest06.java
@@ -18,6 +18,13 @@ package CourseReview.ControlStatement006;
 */
 public class ForTest06 {
 	public static void main(String[] args) {
-		
+		//循环9次
+		for(int i = 1 ; i < 10 ; i++ ) {
+			for(int j = 1 ; j < 10 ; j++) {
+				System.out.print(i + "*" + j + "=" + (i * j));
+				System.out.print("\t");
+			}
+			System.out.println("\n");
+		}
 	}
 }
---
+++ JavaSE/BasicExercise/src/CourseReview/ControlStatement006/HomeWork.java
@@ -0,0 +1,18 @@
+package CourseReview.ControlStatement006;
+/*
+根据指定月份，打印该月份所属的季节。
+	3,4,5 春季 
+	6,7,8 夏季 
+	9,10,11 秋季 
+	12, 1, 2 冬季
+
+	if和switch各写一版
+*/
+import java.util.Scanner;
+public class HomeWork {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("请输入合法的数字【该数字可以是：1 2 3 4 5 6 7 8 9 10 11 12】");
+		int i = s.nextInt();
+	}
+}
