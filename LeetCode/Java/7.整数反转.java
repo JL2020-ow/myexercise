8309908eef8a1732fe8f7ec4402beda56961abee
JavaLearning2020 <williamalex22@126.com>
2020-09-22 18:03:19 +0800

    first commit

--- LeetCode/Java/7.整数反转.java
+++ LeetCode/Java/7.整数反转.java
@@ -11,11 +11,13 @@ class Solution {
         int rev = 0;
         while(x != 0){
             int pop = x % 10;
-            rev = rev * 10 + pop ;
             x /= 10;
-            if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+            if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && pop > Integer.MAX_VALUE % 10)){
+                return 0;
+            }else if(rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && pop < Integer.MIN_VALUE % 10)){
                 return 0;
             }
+            rev = rev * 10 + pop ;
         }
         return (int)rev;
     }
