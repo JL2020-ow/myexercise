0d01abdf9928d2961064f3a9d967fdfd72df760a
JavaLearning2020 <williamalex22@126.com>
2020-09-22 10:18:16 +0800

    first commit

--- LeetCode/Java/两数之和.java
+++ LeetCode/Java/两数之和.java
@@ -1,3 +1,5 @@
+import jdk.tools.jlink.internal.plugins.ExcludePlugin;
+
 /*
  * @lc app=leetcode.cn id=1 lang=java
  *
@@ -6,10 +8,21 @@
 
 // @lc code=start
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-       for(int i=0;i<nums.length;i++){
-           
-       }
-    }
+   public int[] twoSum(int[] nums, int target) {
+      for (int i = 0; i < nums.length; i++) {
+         for (int j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+               return new int[] { i, j };
+            }
+         }
+      }
+      try {
+         throw new Exception("No such sum soultion");
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return null;
+   }
 }
 // @lc code=end
